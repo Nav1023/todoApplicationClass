d7b3c1a642558bdd91a900bd5e87f9676303dec0
Navneet Singh <rathore.singhnav@gmail.com>
2021-09-06 21:42:17 +0530

    created the follow api

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
 
 const userController = {
   create: async (req, res) => {
@@ -83,6 +84,59 @@ const userController = {
       });
     }
     
+  },
+
+  follow: async(req, res) => {
+    // Steps to login api
+    // 1. Get the email and password from request.
+    // 2. Validate the email and password ( whether it is empty/undefined/null)
+    // 3. Fetch the record from DB (database) that matches the email & password.
+    // 4. Condition to check whether the record exists or not.
+    // 5. Accordingly send the response to the client.
+    try{
+      const { userId, followUserId } = req.body;
+
+
+      // let userId = mongoose.Types.ObjectId(req.body.userId);
+      // let followUserId = mongoose.Types.ObjectId(req.body.followUserId);
+
+      const existingUser = await User.findById(userId);
+
+      if(existingUser){
+        const followUser = await User.findById(followUserId);
+        if(followUser){
+          // Date.now()
+          
+          const followObject = {
+            userId: existingUser._id,
+            name: existingUser.firstname,
+            dateTime: "10/20/2019" 
+          }
+          followUser.followers.unshift(followObject);
+          await followUser.save();
+          
+          return res.status(200).send({
+            message: 'followed successfully',
+            status: true
+          }); 
+        }
+
+      } 
+      
+      return res.status(200).send({
+        message: 'Wrong ids',
+        status: false
+      });
+
+    } catch(e){
+      console.log(e);
+      return res.status(400).send({
+        message: 'Something went wrong!! Please try again later',
+        status: false,
+        error: e
+      });
+    }
+    
   }
 }
 
@@ -103,5 +157,27 @@ module.exports = userController;
 // Model.findOneAndDelete()
 // Model.findOneAndUpdate()
 // Model.findOneAndReplace()
+
+
+// Authentication & Authorization
+
+// Authentication
+// 1. Users are having correct credentials. 
+// 2. Then we will say that this user is authenticated (validated)
+
+//  Authorization
+// 1. control the access to multiple internal sub-sytems.
+// 2. example 
+// - students can't see the salary of teachers.
+// - teachers can' see the other teachers salary.
+// - Only admin should have rights to view the salary for teachers.
+
+// Token
+// Jwt (json web tokens) token 
+
+// Create the jwt token  with some additional values.
+// send it to the frontend 
+// then frontend will send that in headers for all the apicalls being made.
+// we will validate that token and process the request
 
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const UserSchema = new mongoose.Schema({
   firstname: {
@@ -26,7 +27,22 @@ const UserSchema = new mongoose.Schema({
     type: Boolean,
     required: true,
     default: true,
-  }
+  },
+  followers: [
+    {
+      userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      dateTime: {
+        type: Date,
+        required: true
+      },
+      name: {
+        type: String
+      }
+    }
+  ]
 
 })
 
@@ -41,3 +57,35 @@ module.exports = User = mongoose.model('user', UserSchema);
 // Array,
 // Object,
 // Decimal
+
+
+
+// SubDocuments
+// Nested Document or Document inside another document or child schema
+
+// {
+//   {
+//     "_id":{"$oid":"612fa27cf7a256a027d1b276"},
+//     "active":true,
+//     "firstname":"navneet",
+//     "email":"testing1@gmail.com",
+//     "password":"1234567",
+//     "age":{"$numberInt":"20"},
+//     "dob":{"$date":{"$numberLong":"1008095400000"}},
+//     "__v":{"$numberInt":"0"}
+//     "followers": [
+//       {
+//         id: {"$oid":"1339488d23bd96a1e617e40"},
+//         name: "testing",
+//         dateTime: {"$date":{"$numberLong":"1008095400000"}}
+//       },
+//       {
+//         id: {"$oid":"1339488d23bd96a1e617e41"},
+//         name: "testing",
+//         dateTime: {"$date":{"$numberLong":"1008095400000"}}
+//       }
+//     ]
+//   }
+// }
+
+
